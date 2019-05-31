b586a2aa5b15e3920746225551f51b1fb85c8d84
paranoidcake <victorpanayotov@gmail.com>
2019-05-31 17:58:08 +0100

    Reintroduced issue-submission as it was in old repo
    Also includes unpushed changes from my local:
    
     - Swapped from MatNativeDateModule to MatMomentDateModule for formatting and improved locale support
     - Introduced getErrorDisplay() as a dynamic function to return a response when a given FormControl has an error of a given type
     - Defined findErrorOf() which maps errors of controls within the submitForm FormGroup and returns an error of a given control name
     - Added mat-error to all mat-form-fields which have validators demonstrating getErrorDisplay()

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,33 @@
-import {BrowserModule} from '@angular/platform-browser';
-import {NgModule} from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
 
-import {AppComponent} from './app.component';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {MatButtonModule, MatCardModule, MatIcon, MatIconModule, MatDrawer, MatSidenavModule, MatSelectModule, MatFormFieldModule, MatOptionModule} from '@angular/material';
-import {FlexLayoutModule} from '@angular/flex-layout';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-import {IssueComponent} from './issue/issue.component';
-import {NavigatorComponent} from './navigator/navigator.component';
-import {AppRoutingModule} from './/app-routing.module';
-import {CommentComponent} from './comment/comment.component';
-import {environment} from '../environments/environment';
-import {AngularFireModule} from '@angular/fire';
-import {AngularFireStorageModule} from '@angular/fire/storage';
-import {AngularFireAuthModule} from '@angular/fire/auth';
-import {AngularFirestoreModule} from '@angular/fire/firestore';
+import { MatButtonModule,
+  MatCardModule,
+  MatIconModule,
+  MatSidenavModule,
+  MatSelectModule,
+  MatFormFieldModule,
+  MatOptionModule,
+  MatInputModule,
+  MatDatepickerModule,
+  MatSlideToggleModule } from '@angular/material';
+
+import { MatMomentDateModule } from '@angular/material-moment-adapter';
+
+import { FlexLayoutModule } from '@angular/flex-layout';
+
+import { IssueComponent } from './issue/issue.component';
+import { NavigatorComponent } from './navigator/navigator.component';
+import { AppRoutingModule } from './/app-routing.module';
+import { CommentComponent } from './comment/comment.component';
+import { environment } from '../environments/environment';
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireStorageModule } from '@angular/fire/storage';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
 import { NavRouteComponent } from './navigator/nav-route/nav-route.component';
 import { HomeComponent } from './home/home.component';
 import { RankedinstitutionsComponent } from './rankedinstitutions/rankedinstitutions.component';
@@ -22,8 +35,9 @@ import { ResolutionComponent } from './issue/resolution/resolution.component';
 import { IssueSubmitComponent } from './issue/issue-submit/issue-submit.component';
 import { Error404Component } from './error404/error404.component';
 import { ReactiveFormsModule } from '@angular/forms';
+import { MAT_DATE_LOCALE } from '@angular/material/core';
 
-import {FirebaseUIModule} from 'firebaseui-angular';
+import { FirebaseUIModule } from 'firebaseui-angular';
 import * as firebase from 'firebase/app';
 import * as firebaseui from 'firebaseui';
 import { LoaderComponent } from './loader/loader.component';
@@ -84,6 +98,10 @@ const firebaseUiAuthConfig: firebaseui.auth.Config = {
     MatFormFieldModule,
     MatSelectModule,
     MatOptionModule,
+    MatInputModule,
+    MatDatepickerModule,
+    MatMomentDateModule,
+    MatSlideToggleModule,
     AngularFireModule.initializeApp(environment.firebase),
     AngularFirestoreModule, // imports firebase/firestore, only needed for database features
     AngularFireAuthModule, // imports firebase/auth, only needed for auth features,
@@ -91,7 +109,9 @@ const firebaseUiAuthConfig: firebaseui.auth.Config = {
     ReactiveFormsModule,
     FirebaseUIModule.forRoot(firebaseUiAuthConfig)
   ],
-  providers: [],
+  providers: [
+    {provide: MAT_DATE_LOCALE, useValue: 'en-GB'},
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
