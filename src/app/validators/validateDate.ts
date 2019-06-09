4226977e6bbd0d2e7c9445edee37add0a335fd7d
Eric Moynihan <git@moynihan.io>
2019-06-09 01:53:14 +0100

    Fixed noder require in frontend

--- src/app/validators/validateDate.ts
+++ src/app/validators/validateDate.ts
@@ -1,6 +1,6 @@
 import { AbstractControl } from '@angular/forms';
 
-const moment = require('moment');
+import * as moment from "moment";
 
 export function validateDate(control: AbstractControl): {[key: string]: any} | null {
   const dateRegExp = new RegExp(/\d{4}-\d{2}-\d{2}.*/);
