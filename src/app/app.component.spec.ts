fdc71adcaa95fbca9eaf1e19eb4fe35fd5a17e37
ein <victorpanayotov@gmail.com>
2019-06-11 15:15:01 +0100

    Removed unwanted change to spec file

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -12,7 +12,7 @@ describe("AppComponent", () => {
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   }));
-  /* it(`should have as title 'fixiversity'`, async(() => {
+  it(`should have as title 'fixiversity'`, async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app.title).toEqual("edu-change");
@@ -24,5 +24,5 @@ describe("AppComponent", () => {
     expect(compiled.querySelector("h1").textContent).toContain(
       "Welcome to edu-change!"
     );
-  })); */
+  }));
 });
